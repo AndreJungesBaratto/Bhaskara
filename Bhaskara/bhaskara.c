c012928a0ada2ad2716e43ba8473519e9df710ff
AndreJungesBaratto <102363867+AndreJungesBaratto@users.noreply.github.com>
2022-04-01 09:42:01 -0300

    Add files via upload

--- Bhaskara/bhaskara.c
+++ Bhaskara/bhaskara.c
@@ -1,19 +1,29 @@
+#include <conio.h>
 #include <stdio.h>
 #include <math.h>
-int main(){
+int main()
+{
 float a, b, c;
-printf("Informe os coeficientes a, b e c da equacao:\n");
-scanf("%f, %f, %f", &a, &b, &c);
+
+    printf("Informe os coeficientes da equacao:\n");
+    scanf("%f, %f, %f", &a, &b, &c);
+
 float delta, x1, x2;
-delta=pow(b,2)-4*a*c;
-    if(delta<0){
-        printf("A equacao nao possui raizes reais.\n");
-    }
+
+    delta=(pow(b, 2)-4*a*c);
+
+    if(delta<0)
+        {
+        printf("Essa equacao nao possui raizes reais\n");
+        }
     else{
-x1=(-b+sqrt(delta))/2*a;
-x2=(-b-sqrt(delta))/2*a;
-printf("Raizes da equaçao:\n");
-printf("X = %f\n", x1);
-printf("X' = %f\n", x2);
-}
+        x1=(-b+sqrt(delta))/2*a;
+        x2=(-b-sqrt(delta))/2*a;
+
+            printf("As Raizes da equacao sao:\n");
+            printf("X = %f\n", x1);
+            printf("X' = %f\n", x2);
+        }
+    getch();
+return 0;
 }
